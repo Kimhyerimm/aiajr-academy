e1a4c3613ee911baf168ef9d74c9da362858404d
kas4947 <63032593+kas4947@users.noreply.github.com>
2020-07-03 17:53:38 +0900

    20200703

--- jsp/FirstWeb/src/first/NowServlet.java
+++ jsp/FirstWeb/src/first/NowServlet.java
@@ -9,35 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class NowServlet
- */
-@WebServlet("/Now")
+@WebServlet("/now")
 public class NowServlet extends HttpServlet {
-  
 
-   /**
-    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-    */
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-         throws ServletException, IOException {
-         int total = 0;
-         for (int cnt = 1; cnt < 101; cnt++)
-         total += cnt;
-         PrintWriter out = response.getWriter();
-         out.println( "<HTML> ");
-         out.println( "<HEAD><TITLE>Hundred Servlet</TITLE></HEAD> ");
-         out.println( "<BODY> ");
-         out.printf( "1 + 2 + 3 + ... + 100 = %d ", total);
-         out.println( "</BODY> ");
-         out.println( "</HTML> ");
-         }
-   
-   
-   
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      // TODO Auto-generated method stub
-      doGet(request, response);
-   }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int total = 0;
+		for (int cnt = 1; cnt < 101; cnt++)
+			total += cnt;
+		PrintWriter out = response.getWriter();
+		out.println("<HTML> ");
+		out.println("<HEAD><TITLE>Hundred Servlet</TITLE></HEAD> ");
+		out.println("<BODY> ");
+		out.printf("1 + 2 + 3 + ... + 100 = %d ", total);
+		out.println("</BODY> ");
+		out.println("</HTML> ");
+	}
 
 }
